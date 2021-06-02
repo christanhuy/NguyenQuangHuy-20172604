9cf0d9c0ad91d14ff7636fd7d6fc25dc5a0a8541
Nguyễn Quang Huy <83380044+christanhuy@users.noreply.github.com>
2021-06-02 08:58:57 +0700

    Update reaction.c

--- cs140-ps0/reaction.c
+++ cs140-ps0/reaction.c
@@ -1,8 +1,5 @@
 #include "pintos_thread.h"
 
-// Forward declaration. This function is implemented in reaction-runner.c,
-// but you needn't care what it does. Just be sure it's called when
-// appropriate within reaction_o()/reaction_h().
 void make_water();
 
 struct reaction {
